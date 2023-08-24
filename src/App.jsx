9343aa94bfcfaefe70ab04f456572465d895fc38
meow0222 <meow222.csgo@gmail.com>
2023-08-24 14:23:54 +0900

    Make GET and POST /mao

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Todo from './components/Todo';
 import AddForm from './components/AddForm';
-import Container from './components/Container'
+import Container from './components/Container';
 
 import './App.css';
 
@@ -11,7 +11,7 @@ const App = () => {
 
   useEffect(() => {
     // JSONファイルからtodosを読み込む
-    fetch('todos.json')
+    fetch('http://localhost:8080/api/todos')
       .then(response => response.json())
       .then(data => setTodos(data))
       .catch(error => console.error('Error loading todos:', error));
@@ -20,7 +20,7 @@ const App = () => {
   const updateTodos = (newTodos) => {
     setTodos(newTodos);
     // JSONファイルにtodosを保存
-    fetch('todos.json', {
+    fetch('http://localhost:8080/api/todos', {
       method: 'PUT',
       body: JSON.stringify(newTodos),
       headers: {
@@ -42,14 +42,39 @@ const App = () => {
   };
 
   const handleAddFormSubmit = (title) => {
-    const newTodos = [...todos];
-    newTodos.push({
+    const newTodo = {
       id: Date.now(),
       title: title,
       isCompleted: false,
+    };
+    console.log(newTodo);
+    fetch('http://localhost:8080/api/todos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newTodo),
+    })
+    .then(response => {
+      console.log("response", response);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      // 新しいToDoをリストに追加
+      const updatedTodos = [...todos, data];
+      updateTodos(updatedTodos);
+    })
+    .catch(error => {
+      console.error('Error adding todo:', error);
     });
-    updateTodos(newTodos);
   };
+  
+  
+
+  
 
   const handleTodoCheckboxChange = (id) => {
     const newTodos = todos.map((todo) => {
@@ -72,6 +97,7 @@ const App = () => {
     updateTodos(newTodos);
   };
 
+  /*
   const todoItems = todos.map((todo) => {
     return (
       <Todo
@@ -81,17 +107,33 @@ const App = () => {
         onCheckboxChange={handleTodoCheckboxChange}
       />
     );
-  });
-
+  });*/
+{console.log(`todos`,todos)}
   return (
     <Container>
+
+
+      
+
       <h1>
         My TODOs
-        <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent roundeds' onClick={handlePurgeClick}>Purge</button>
+        <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent roundeds' 
+        onClick={handlePurgeClick}>Purge</button>
       </h1>
-      <ul id="todos">
-        {todoItems}
-      </ul>
+      {/* <ul id="todos"> */}
+        {/* {todoItems} */}
+        {todos.map(todo => (
+          <div className='bg-blue'
+          key={todo.id}>
+            {todo?.task1?.title}
+            {todo?.task2?.title}
+            {todo?.task3?.title}
+            
+          </div>
+        ))}
+
+
+      {/* </ul> */}
       <AddForm
         onSubmit={handleAddFormSubmit}
       />
--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -24,7 +24,7 @@ const AddForm = (props) => {
           ref={inputRef}
           className=''
         />
-        <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded p-'>Add</button>
+        <button type='submit' className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded p-'>Add</button>
         
       </form>
     );
