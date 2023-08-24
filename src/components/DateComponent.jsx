51a2d3fadeb2294f027216840a4f482ba6142fb4
hyerim <kimhyerim511@gmail.com>
2023-08-24 14:20:52 -0700

    add event function

--- src/components/DateComponent.jsx
+++ src/components/DateComponent.jsx
@@ -1,15 +1,29 @@
+import { useState } from "react";
+
 function DateComponent(props) {
+    
     let date = new Date(props.today)
     let checkHoliday = ''
+    let checkEvent = props.checkEvent
+    let eventColor = {
+        color: "inherit"
+    }
+    let eventDetail = props.detail
+    let showingDetail = ""
 
     if(date.getDay() === 0) {
-        checkHoliday = "holiday"
-    } else {
-        checkHoliday = "day"
+        eventColor.color = "red"
+    } 
+
+    for(let i=0; i<checkEvent.length; i++){
+        if(checkEvent[i].getDate() === date.getDate()) {
+            eventColor.color = "yellow"
+            showingDetail = showingDetail + " \uD83D\uDC04 " + eventDetail[i]
+        } 
     }
 
     return (
-        <li className={ checkHoliday }>{ date.getDate() }</li>
+        <li className={ checkHoliday } style={eventColor} onClick={props.onClick} value={date.getDate()}>{ date.getDate() } <span>{showingDetail}</span></li>
     )
 }
 
--- src/components/DateContainer.jsx
+++ src/components/DateContainer.jsx
@@ -2,65 +2,114 @@ import DateComponent from './DateComponent'
 import { useState, useEffect, useRef } from "react";
 
 function DateContainer(props) {
+    let [eventDay, newEventDay] = useState("")
+    let [showDate, setModalDate] = useState("")
+    const [isModal, setModal] = useState(false);
+    const [eventData, getEvent] = useState("");
+    let eventDate = new Date()
 
-    const [newEvent, setEvent] = useState("");
-    const eventRef = useRef(null);
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        props.onSubmit(newEvent);
-        // console.log(e.target[0].value)
+    useEffect(() => {
+        fetch("http://localhost:8080/api/events", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+        .then((res) => res.json())
+        .then((data) => getEvent(Object.values(data[0])));
+    }, []);
 
-        inputVal = { events: e.target[0].value}
+    const tempArr = []
+    for(let i=0; i<eventData.length; i++) {
+        tempArr.push(eventData[i].eventday)
+    }
+    const tempDetail = []
+    for(let i=0; i<eventData.length; i++) {
+        tempDetail.push(eventData[i].detail)
+    }
     
-        // const form = e.target;
-        // const formData = new FormData(form);
 
 
-        useEffect(() => {
-            fetch("http://localhost:8080/addEvent", {
-                method: "POST",
-                headers: {
-                    "casino": "addEvent",
-                    "Content-type": "application/json"
-                },
-                body: JSON.stringify(inputVal)
-            })
-            .then((res) => res.text(inputVal))
-            .then((data) => setEvent(data));
-        }, []);
-        console.log(inputVal);
-        
-        // const formJson = Object.fromEntries(formData.entries());
-        // console.log(formJson);
-      }
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    function getDate(e) {
+        eventDate = new Date(dateArr[0].getYear(), dateArr[0].getMonth(), e.target.value)
+        newEventDay(eventDate)
+        setModal(true)
+        let newEvent = months[eventDate.getMonth()] + " " + eventDate.getDate()
+        setModalDate(newEvent)
+    }
+
+    const addNewEvent = (e) => {
+        e.preventDefault();
+
+        const newEvent = {
+            id: Date.now(),
+            eventday: showDate,
+            detail: e.target[0].value
+        }
+        fetch('http://localhost:8080/api/events', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newEvent),
+        })
+        .then(response => {
+        console.log("response", response);
+        if (!response.ok) {
+            throw new Error('EVENT response was not ok');
+        }
+        return response.json();
+        })
+        .then(data => {
+        console.log("Check Data",data);
+        })
+        .catch(error => {
+        console.error('Error adding todo:', error);
+        });
+    }
 
     let dateArr = props.dateArr;
     let thisMonth = new Date(dateArr[0]);
     let thisMonthStart = thisMonth.getDay();
-    let inputVal;
-
-    function blankDate(num) {
-        return [...Array(num)].map((_, index) => <tr key={index}/>) 
-    }
-
     
 
+    function blankDate(num) {
+        return [...Array(num)].map((_, index) => <li key={index}/>) 
+    }
+
+    const needCheck = tempArr.map((date)=>{
+        return new Date(date + ", 2023")
+    })    
+
     return (
-        <ul className="day-container">
-            {blankDate(thisMonthStart)}
+        <div>
+            <ul className="day-container">
+                {blankDate(thisMonthStart)}
+                {
+                    dateArr.map((date) => {
+                        return <DateComponent today={date} checkEvent={needCheck} detail={tempDetail} key={dateArr[date]} onClick={getDate} />
+                    })
+                }
+            </ul>
             {
-                dateArr.map((date) => {
-                    return <DateComponent today={date} key={dateArr[date]} />
-                })
-            }
-            <div className="modal">
-                <form method="post" onSubmit={handleSubmit}>
-                    <input type="text" name="eventText" id="eventText" value={newEvent} ref={eventRef}/>
+            isModal 
+            ? <div className="modal block">
+                <form method="post" onSubmit={addNewEvent}>
+                    <p>{showDate}</p>
+                    <input type="text" name="eventText" id="eventText" />
                     <button type="submit">Add Event</button>
                 </form>
-            </div>            
-        </ul>
+                </div>
+            : <div className="modal">
+                <form method="post" onSubmit={addNewEvent}>
+                    <input type="text" name="eventText" id="eventText" />
+                    <button type="submit">Add Event</button>
+                </form>
+            </div>}          
+        </div>
     )
 }
 
